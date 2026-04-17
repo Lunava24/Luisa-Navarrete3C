86f6701c0b0aae9e9052dc12be1d4825c36d93c7
Lu <navarreteluisa71@gmail.com>
2026-04-17 17:29:00 +0000

    sss

---
+++ js-events/script.js
@@ -0,0 +1,16 @@
+// function greet(name) {
+//     //console.log(`Hello ${name}`);
+//     console.log("Hello "+ name);
+// }
+
+// greet("Luisa");
+
+// function addNumbers(num1, num2) {
+//     let sum = num1 + num2;
+//     alert("Sum is equal to" + sum);
+// }
+
+//addNumbers(6,7);
+
+
+function 
