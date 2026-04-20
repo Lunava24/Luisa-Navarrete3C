a2c4e777e272e745a39b117dded1cf6ce252c623
Lu <navarreteluisa71@gmail.com>
2026-04-20 17:29:10 +0000

    gdgd

--- js-events/script.js
+++ js-events/script.js
@@ -13,4 +13,14 @@
 //addNumbers(6,7);
 
 
-function 
+function scripture1() {
+    alert("We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost.")
+}
+
+function scripture2() {
+    alert("We believe that men will be punished for their own sins, and not for Adam’s transgression.")
+}
+
+function scripture3() {
+    alert("We believe that through the Atonement of Christ, all mankind may be saved, by obedience to the laws and ordinances of the Gospel.")
+}
