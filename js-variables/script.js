f0ffdabf6589d0caa79e3e26d9ae52274bf27fce
Lu <navarreteluisa71@gmail.com>
2026-04-22 20:19:21 +0000

    dfd

---
+++ js-variables/script.js
@@ -0,0 +1,8 @@
+function function() {
+    const firstName = "Luisa";
+    const favoriteColor = "Blue";
+    let currentLocation = "México";
+    let mood = "Lazy";
+    console.log(`I'm ${firstName}, I live in ${currentLocation}, my favorite color is ${favoriteColor}, and right now I feel ${mood}.`);
+    currentLocation = "casas grandes";
+}
