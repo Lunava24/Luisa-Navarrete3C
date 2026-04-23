2abd8f65473981124fdbe4ab3d3793421eee7792
Lu <navarreteluisa71@gmail.com>
2026-04-23 19:56:41 +0000

    o

--- js-variables/script.js
+++ js-variables/script.js
@@ -8,3 +8,17 @@ function function1() {
     mood = "Happy"
     console.log(`I'm ${firstName}, I live in ${currentLocation}, my favorite color is ${favoriteColor}, and right now I feel ${mood}.`);
 }
+
+function function2() {
+    const companyName = "Dairy Queen";
+    const foundingYear = "1940";
+    let isActive = true;
+    let fundingAmount = undefined;
+    console.log(`I work in ${companyName}, it was found at ${foundingYear}, I'm actually ${isActive} and ${fundingAmount}`);
+}
+
+function function3() {
+    let number = 5;
+    number = (((number * 2) + 10) / 2) - number;
+    alert(number);
+}
