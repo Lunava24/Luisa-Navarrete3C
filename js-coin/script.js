f3716f6320b39ecf8ab81dc2213e741cc19b00c2
Lu <navarreteluisa71@gmail.com>
2026-04-29 20:18:55 +0000

    lll

--- js-coin/script.js
+++ js-coin/script.js
@@ -4,15 +4,37 @@
 //     console.log("positive number");
 // }
 //  console.log("nice number");
+let userSelection = null;
+
+function tail() {
+    userSelection = "Tails";
+}
+
+function head() {
+    userSelection = "Head";
+}
 
 function clicks() {
     let coin = Math.floor(Math.random() * 2);
     let result = document.getElementById("flips");
 
     if (coin === 0) {
-        result.innerText = 'Heads';
+        result.innerText = 'Heads'; if (userSelection === "Head") {
+            alert("You won :DDDDDDDDDDD");
+            userSelection = null;
+        }   else {
+            alert("You lost ;v")
+            userSelection = null;
+        }
+
     } else {
-        result.innerText = "Tails";
+        result.innerText = "Tails"; if (userSelection === "Tails") {
+            alert("You won");
+            userSelection = null;
+        }   else {
+            alert("You lost ;v")
+            userSelection = null;
+        }
     }
 }
 
