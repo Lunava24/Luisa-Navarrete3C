91ab3eaf831911468f8d46b6a36cd5d3389f283c
Lu <navarreteluisa71@gmail.com>
2026-05-11 19:38:53 +0000

    jjjj

---
+++ interval/script.js
@@ -0,0 +1,14 @@
+function time
+
+let counter = 0;
+
+function count() {
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button').onclick = function(){
+    setInterval(count, 1000);
+    };
+});
